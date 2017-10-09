6dc370337619faf85aa3bdff4f07e4a5d19d948d
Natasha7397 <natufochka@yandex.ru>
2017-10-09 09:07:05 +0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<head>
+<title>Отдел кадров</title>
+<link rel="shortcut icon" href="/images/myspace.ico" type="image/x-icon">
+
+<link href="style.css" rel="stylesheet" type="text/css" media="screen" />
+</head>
+<body>
+<div id="header-wrapper">
+	<div id="header" class="container">
+		<div id="logo">
+			<h1><a href="#">Отдел кадров</a></h1>
+			
+		</div>
+		<div id="menu">
+			<ul>
+				<li class="current_page_item">
+                                    <a href="#">Главная</a></li>
+				<li><a href="#">Вакансии</a></li>
+                               <li><a href="#">Документы</a></li>
+				<li><a href="#">Справки</a></li>
+			</ul>
+		</div>
+	</div>
+	<div id="banner" class="container">
+		<div class="image-style">
+                    <img src="images/img03.png" width="970" height="320" alt="" /></div>
+	</div>
+</div>
+<div id="wrapper">
+	<div id="splash" class="container">Отдел кадров является не только функциональной единицей, это еще и лицом компании, 
+            так как именно в отделе кадров любой соискатель начинает знакомиться с организацией. </div>
+	<div id="page" class="container">
+		<div id="content">
+			<div class="post">
+				<h2 class="title"><a href="#">Вас приветствует отдел кадров</a></h2>
+				<div class="entry">
+					<p>Отдел кадров способствовует достижению целей организации путем 
+                                            обеспечения предприятия необходимыми кадрами и эффективного использования потенциала работников.</p> 
+                    <p>Подбор работников ведется с помощью специально разработанных стратегий: подачи информации о вакансиях в СМИ и службы занятости населения, 
+                        применения методик отбора, тестирования, процедур адаптации специалистов и последующего повышения квалификации.</p>
+				</div>
+			</div>
+			
+		</div>
+		<div id="sidebar">
+			<div>
+				<h2>Мы можем</h2>
+				<ul class="list-style1">
+					<li class="first"><a href="#">Принять на работу</a></li>
+					<li><a href="#">Оформить документы для отпуска</a></li>
+					<li><a href="#">Составить спрвку №8</a></li>
+					<li><a href="#">Закрыть больничный</a></li>
+					<li><a href="#">Найти нового сотрудника</a></li>
+					<li><a href="#">Ответь на любой Ваш вопрос</a></li>
+				</ul>
+			</div>
+		</div>
+		
+	</div>
+	
+</div>
+<div id="footer-content" class="container">
+	<div id="footer-bg">
+		
+		<div id="column2">
+			<ul class="list-style2">
+				<li class="first"><a href="#">Подать резюме</a></li>
+				<li><a href="#">Оформить больничный</a></li>
+				<li><a href="#">Оформить отпуск</a></li>
+				<li><a href="#">Оформить командировку</a></li>
+				<li><a href="#">Заказать справку</a></li>
+			</ul>
+		</div>
+		<div id="column3">
+			
+			<ul class="list-style2">
+				<p>Режим работы: пн-пт 8:00 - 18:00<br> 
+                    Телефон: 8(945)123-45-69<br>
+                     E-mail: otdel@yandex.ru</p>
+			</ul>
+		</div>
+	</div>
+</div>
+</body>
+</html>
